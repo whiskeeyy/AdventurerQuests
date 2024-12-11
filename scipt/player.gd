da37311b30b3cc7bfbfd02b217bb884f0050cb16
whiskeeyy <kietduongdinh@gmail.com>
2024-12-12 06:00:11 +0700

    movement update

--- scipt/player.gd
+++ scipt/player.gd
@@ -1,15 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @onready var animated_sprite = $AnimatedSprite2D
-
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
